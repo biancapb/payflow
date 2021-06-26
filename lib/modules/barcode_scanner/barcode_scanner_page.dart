6369dee2779e372adfa5737bd60a0caf0015e81e
Bianca Barbosa <yume_shiro@hotmail.com>
2021-06-26 11:56:49 -0300

    add divider and rotating label

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/bottom_sheet/bottom_sheet_widget.dart';
 import 'package:payflow/shared/widgets/divider_vertical/divider_vetical.dart';
 import 'package:payflow/shared/widgets/label_button/label_button.dart';
 import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
@@ -15,6 +16,14 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
+    return BottomSheetWidget(
+      title: "Não foi possível identificar um código de barras",
+      subtitle: "Tente escanear novamente ou digite o código do seu boleto",
+      primaryLabel: "Escanear novamente",
+      primaryOnPressed: () {},
+      secondaryLabel: "Digitar código",
+      secondaryOnPressed: () {},
+    );
     return RotatedBox(
       quarterTurns: 1,
       child: SafeArea(
--- lib/shared/widgets/set_label_buttons/set_label_buttons.dart
+++ lib/shared/widgets/set_label_buttons/set_label_buttons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/divider_vertical/divider_vetical.dart';
 import 'package:payflow/shared/widgets/label_button/label_button.dart';
 
@@ -7,13 +8,15 @@ class SetLabelButtons extends StatelessWidget {
   final VoidCallback primaryOnPressed;
   final String secondaryLabel;
   final VoidCallback secondaryOnPressed;
-  const SetLabelButtons(
-      {Key? key,
-      required this.primaryLabel,
-      required this.primaryOnPressed,
-      required this.secondaryLabel,
-      required this.secondaryOnPressed})
-      : super(key: key);
+  final bool enablePrimaryColor;
+  const SetLabelButtons({
+    Key? key,
+    required this.primaryLabel,
+    required this.primaryOnPressed,
+    required this.secondaryLabel,
+    required this.secondaryOnPressed,
+    this.enablePrimaryColor = false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +26,16 @@ class SetLabelButtons extends StatelessWidget {
         children: [
           Expanded(
               child: LabelButton(
-                  label: primaryLabel, onPressed: primaryOnPressed)),
+            label: primaryLabel,
+            onPressed: primaryOnPressed,
+            style: enablePrimaryColor ? TextStyles.buttonPrimary : null,
+          )),
           DividerVerticalWidget(),
           Expanded(
               child: LabelButton(
-                  label: secondaryLabel, onPressed: secondaryOnPressed)),
+            label: secondaryLabel,
+            onPressed: secondaryOnPressed,
+          )),
         ],
       ),
     );
