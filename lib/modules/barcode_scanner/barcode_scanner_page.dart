46018c50baa8131f28aebc4b3da495d3b9dafdb2
Bianca Barbosa <yume_shiro@hotmail.com>
2021-06-26 00:13:26 -0300

    creating rotate box

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/divider_vertical/divider_vetical.dart';
 import 'package:payflow/shared/widgets/label_button/label_button.dart';
+import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
   const BarcodeScannerPage({Key? key}) : super(key: key);
@@ -13,43 +15,50 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: Text(
-            "Escaneie o código e barras do boleto",
-            style: TextStyles.buttonBackground,
+    return RotatedBox(
+      quarterTurns: 1,
+      child: SafeArea(
+        top: true,
+        bottom: true,
+        left: true,
+        right: true,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Text(
+              "Escaneie o código e barras do boleto",
+              style: TextStyles.buttonBackground,
+            ),
+            centerTitle: true,
+            leading: BackButton(
+              color: AppColors.background,
+            ),
           ),
-          leading: BackButton(
-            color: AppColors.background,
+          body: Column(
+            children: [
+              Expanded(
+                  child: Container(
+                color: Colors.black.withOpacity(0.6),
+              )),
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.transparent,
+                  )),
+              Expanded(
+                  child: Container(
+                color: Colors.black.withOpacity(0.6),
+              ))
+            ],
+          ),
+          bottomNavigationBar: SetLabelButtons(
+            primaryLabel: "Inserir código do boleto",
+            primaryOnPressed: () {},
+            secondaryLabel: "Adicionar da galeria",
+            secondaryOnPressed: () {},
           ),
         ),
-        body: Column(
-          children: [
-            Expanded(
-                child: Container(
-              color: Colors.black,
-            )),
-            Expanded(
-                flex: 2,
-                child: Container(
-                  color: Colors.transparent,
-                )),
-            Expanded(
-                child: Container(
-              color: Colors.black,
-            ))
-          ],
-        ),
-        bottomNavigationBar: Row(
-          children: [
-            Expanded(
-                child: LabelButton(
-                    label: "Inserir código do boleto", onPressed: () {})),
-            Expanded(
-                child: LabelButton(
-                    label: "Adicionar da galeria", onPressed: () {})),
-          ],
-        ));
+      ),
+    );
   }
 }
